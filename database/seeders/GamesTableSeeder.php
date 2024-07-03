a67cb70db9da1fefcf30dc60bcdfe9ae3448f6ac
Luca <luca@lambia.it>
2024-07-03 13:59:26 +0200

    add a seeder

--- database/seeders/GamesTableSeeder.php
+++ database/seeders/GamesTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Game;
+use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -11,18 +12,27 @@ class GamesTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        for ($i=0; $i < 10; $i++) { 
+        //Creo UN gioco
+        $newRecord = new Game();
+        $newRecord->name = "Max Payne";
+        $newRecord->price = 9.99;
+        $newRecord->release_year = 2001;
+        $newRecord->cover_image = "https://image.api.playstation.com/cdn/EP1004/CUSA04488_00/wkULSyjvjH6xGS9juseLnkjmtG6VGlem.png?w=440";
+        $newRecord->vote = 10;
+        $newRecord->save();
 
+        //Poi, per 11 volte...
+        for ($i=0; $i < 11; $i++) { 
+            //Creo UN gioco
             $newRecord = new Game();
-            $newRecord->name = "Max Payne";
-            $newRecord->price = 9.99;
-            $newRecord->release_year = 2001;
-            $newRecord->cover_image = "https://image.api.playstation.com/cdn/EP1004/CUSA04488_00/wkULSyjvjH6xGS9juseLnkjmtG6VGlem.png?w=440";
-            $newRecord->vote = 10;
+            $newRecord->name = $faker->sentence(4);
+            $newRecord->price = $faker->randomFloat(2, 0.99, 69.99);
+            $newRecord->release_year = $faker->year(); // oppure $faker->numberBetween(1981, 2024);
+            $newRecord->cover_image = "https://picsum.photos/id/" . $faker->numberBetween(1, 1000) . "/540/960";
+            $newRecord->vote = $faker->randomDigit(); //equivalente di $faker->numberBetween(0, 9);
             $newRecord->save();
-
         }
     }
 }
